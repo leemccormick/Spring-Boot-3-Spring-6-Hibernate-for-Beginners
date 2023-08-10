619b196ba70cac7e5c8e2ff4c25583790a5c7a27
Lee McCormick <lee.mccormick@dialexa.com>
2023-08-10 16:53:30 -0500

    Finished following tutorial section1

---
+++ Section_1_Spring_Boot_3_Quick_Start/01-spring-boot-overview/04-actuator-security-demo/src/test/java/com/leemccormick/springboot/demo/MyCoolApp/MyCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leemccormick.springboot.demo.MyCoolApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
