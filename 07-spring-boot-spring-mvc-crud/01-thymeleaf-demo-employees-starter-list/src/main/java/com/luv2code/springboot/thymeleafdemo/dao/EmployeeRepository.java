82d73ac57860da302679af8c0b3beeec55c2e56b
Lee McCormick <lee.mccormick@dialexa.com>
2023-08-16 13:11:48 -0500

    Finished Section7

---
+++ 07-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+    // add a method to sort by last name --> Using Spring Data JPA to look for a specific format and pattern creates appropriate query
+    public List<Employee> findAllByOrderByLastNameAsc();
+}
